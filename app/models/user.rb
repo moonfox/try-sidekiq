fb752a6742ff6e4d4b8e3eef3d2dff329c730122
sleepmoon.jia <sleepmoon.jia@gmail.com>
2016-04-24 00:00:31 +0800

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# 20.times {User.delay_for(10).create_user("Jack", 20)}
+# 10.times{OurWorker.perform_in(12,"hard")}
+# User.delay_for(10) 会把任务放到默认队列,sidekiq会在User类方法中混入delay,delay_for,delay_until方法
+
 class User
   class << self
     def create_user(name, age)
