99ce631e1df3c99dbdfeb6fd999523cff28a5ec5
sleepmoon.jia <sleepmoon.jia@gmail.com>
2016-04-24 00:17:49 +0800

    add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
-# 20.times {User.delay_for(10).create_user("Jack", 20)}
-# 10.times{OurWorker.perform_in(12,"hard")}
-# User.delay_for(10) 会把任务放到默认队列,sidekiq会在User类方法中混入delay,delay_for,delay_until方法
+# 总结: Delayed extensions(让普通类,非Worker类,拥有delay功能)
+# 1. User.delay_for(10).create_user("Jack", 20) 会把任务放到默认队列
+#   sidekiq会在User类方法中混入delay,delay_for,delay_until方法
+# 2. User.delay_for(10, :retry => false, :queue => 'low').create_user("Jack", 20)
+#   可以为delay_xxx方法指定sidekiq选项:job所在队列,是否进行重试等
+# 3. Sidekiq.remove_delay! 禁用Delayed extensions扩展(只对 Rails application 有效)
 
 class User
   class << self
